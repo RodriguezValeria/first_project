76c45c83a7b70ad4934dfe3d033cb0302813580e
RodriguezValeria <valerodriguez2425@gmail.com>
2024-09-08 23:03:20 -0600

    Se agrega comando Lista Paises

---
+++ Mi primer proyecto/frontend-lab/src/components/ListaPaises.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="display-4 text-center">Lista de países</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ListaPaises",
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
